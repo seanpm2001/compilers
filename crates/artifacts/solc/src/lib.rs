d9e730675795d1acfc0c85550a4c8d566f62bf54
Federico Gimenez <federico.gimenez@gmail.com>
2024-05-31 16:15:52 +0200

    feat: add Prague evm version

--- crates/artifacts/solc/src/lib.rs
+++ crates/artifacts/solc/src/lib.rs
@@ -39,7 +39,7 @@ use foundry_compilers_core::{
     error::SolcError,
     utils::{
         BERLIN_SOLC, BYZANTIUM_SOLC, CANCUN_SOLC, CONSTANTINOPLE_SOLC, ISTANBUL_SOLC, LONDON_SOLC,
-        PARIS_SOLC, PETERSBURG_SOLC, SHANGHAI_SOLC,
+        PARIS_SOLC, PETERSBURG_SOLC, PRAGUE_EOF_SOLC, PRAGUE_SOLC, SHANGHAI_SOLC,
     },
 };
 pub use serde_helpers::{deserialize_bytes, deserialize_opt_bytes};
@@ -810,6 +810,7 @@ pub enum EvmVersion {
     Shanghai,
     #[default]
     Cancun,
+    Prague,
 }
 
 impl EvmVersion {
@@ -819,8 +820,11 @@ impl EvmVersion {
         if *version >= BYZANTIUM_SOLC {
             // If the Solc version is the latest, it supports all EVM versions.
             // For all other cases, cap at the at-the-time highest possible fork.
-            let normalized = if *version >= CANCUN_SOLC {
+
+            let normalized = if *version >= PRAGUE_SOLC {
                 self
+            } else if self >= Self::Cancun && *version >= CANCUN_SOLC {
+                Self::Cancun
             } else if self >= Self::Shanghai && *version >= SHANGHAI_SOLC {
                 Self::Shanghai
             } else if self >= Self::Paris && *version >= PARIS_SOLC {
@@ -861,6 +865,7 @@ impl EvmVersion {
             Self::Paris => "paris",
             Self::Shanghai => "shanghai",
             Self::Cancun => "cancun",
+            Self::Prague => "prague",
         }
     }
 
@@ -929,6 +934,7 @@ impl FromStr for EvmVersion {
             "paris" => Ok(Self::Paris),
             "shanghai" => Ok(Self::Shanghai),
             "cancun" => Ok(Self::Cancun),
+            "prague" => Ok(Self::Prague),
             s => Err(format!("Unknown evm version: {s}")),
         }
     }
@@ -2011,6 +2017,11 @@ mod tests {
             ("0.8.24", EvmVersion::Homestead, Some(EvmVersion::Homestead)),
             ("0.8.24", EvmVersion::Shanghai, Some(EvmVersion::Shanghai)),
             ("0.8.24", EvmVersion::Cancun, Some(EvmVersion::Cancun)),
+            // Prague
+            ("0.8.26", EvmVersion::Homestead, Some(EvmVersion::Homestead)),
+            ("0.8.26", EvmVersion::Shanghai, Some(EvmVersion::Shanghai)),
+            ("0.8.26", EvmVersion::Cancun, Some(EvmVersion::Cancun)),
+            ("0.8.26", EvmVersion::Prague, Some(EvmVersion::Prague)),
         ] {
             let version = Version::from_str(solc_version).unwrap();
             assert_eq!(
